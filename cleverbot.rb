f538692c3e1dcf3a76a0ecae7dda97987f9c0140
andrei512 <puni.andrei23@gmail.com>
2014-01-16 02:28:35 +0200

    Update cleverbot.rb

--- cleverbot.rb
+++ cleverbot.rb
@@ -5,14 +5,14 @@ require 'cleverbot'
  
 def say message, voice="Vicki"
   puts message
-	`say -v #{voice} "#{message}"`
+  `say -v #{voice} "#{message}"`
 end
  
 def chat alice, bob, message, first_voice="Vicki", second_voice="Zarvox"
-	message = message.gsub("*","")
-	say message, first_voice
-	next_message = alice.write(message)
-	chat bob, alice, next_message, second_voice, first_voice
+  message = message.gsub("*","")
+  say message, first_voice
+  next_message = alice.write(message)
+  chat bob, alice, next_message, second_voice, first_voice
 end
  
 chat @client1, @client2, ARGV.count > 0 ? ARGV[0] : "Hi."
